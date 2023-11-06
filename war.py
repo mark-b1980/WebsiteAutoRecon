29e21200175ddea506c1d314f182d2af5505ca5a
mark-b1980 <mark.b@post.cz>
2023-11-06 17:00:36 +0100

    Added DNS and DNS zonetransfer function

--- war.py
+++ war.py
@@ -1,6 +1,8 @@
 import whois
-import pprint
 import requests
+
+import dns.zone
+import dns.resolver
 
 from bs4 import BeautifulSoup
 from colorama import init, Fore, Style
@@ -16,7 +18,7 @@ from colorama import init, Fore, Style
 # 4) Check if WORDLIST is readable and display error if not
 
 URL = "http://192.168.1.2"
-URL = "https://stackoverflow.com/"
+URL = "http://ZoneTransfer.me"
 EXTS = "zip,bz2,tar,gz,tgz,tar.bz2,tar.gz,old,bak,inc,ini,xml,txt,yaml,yml,conf,cnf,config,json".split(",")
 WORDLIST = ""
 
@@ -61,7 +63,7 @@ session.headers.update({'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)
 ########################################################################################
 # LOG OUTPUT
 ########################################################################################
-domain = URL.split("/")[2]
+domain = URL.split("/")[2].lower()
 with open(f"{domain}.log", "w", encoding="UTF-8") as log_file:
 
     ########################################################################################
@@ -81,13 +83,13 @@ with open(f"{domain}.log", "w", encoding="UTF-8") as log_file:
     for line in whois_info.split("\n"):
         line = line.lstrip()
 
-        # Format headline
+        # Format key
         if ": " in line:
             tmp = line.split(":")
             tmp[0] = tmp[0].upper() + ":"
             line = f"{tmp[0]:30}{tmp[1]}"
         
-        # Indention by 2 spaces if there is no headline
+        # Indention by 2 spaces if there is no key
         else:
             line = "  " + line
         
@@ -96,16 +98,65 @@ with open(f"{domain}.log", "w", encoding="UTF-8") as log_file:
     print_info(out)
     print_info()
 
+
+    ########################################################################################
+    # Get DNS data
+    ########################################################################################
+    print_info("DNS:")
+    print_info("="*48)
+    entries = ["A", "A6", "AAAA", "AFSDB", "ANY", "APL", "AXFR", "CAA", "CDNSKEY", "CDS", "CERT", "CNAME", "CSYNC", "DHCID", "DLV", "DNAME", "DNSKEY", "DS", "EUI48", "EUI64", "GPOS", "HINFO", "HIP", "IPSECKEY", "ISDN", "IXFR", "KEY", "KX", "LOC", "MAILA", "MAILB", "MB", "MD", "MF", "MG", "MINFO", "MR", "MX", "NAPTR", "NONE", "NS", "NSAP", "NSAP-PTR", "NSEC", "NSEC3", "NSEC3PARAM", "NULL", "NXT", "OPT", "PTR", "PX", "RP", "RRSIG", "RT", "SIG", "SOA", "SPF", "SRV", "SSHFP", "TA", "TKEY", "TLSA", "TSIG", "TXT", "UNSPEC", "URI", "WKS", "X25"]
+
+    for entry in entries:
+        found = False
+        try:
+            answers = dns.resolver.query(domain, entry)
+            for rdata in answers:
+                found = True
+                key = entry + ":"
+                print_info(f"{key:15} {rdata.to_text()}")
+    
+        except Exception as e:
+            pass
+        
+        if found:
+            print_info()
+    
+    print_info()
+    
+
+    ########################################################################################
+    # Get DNS zonetransfer
+    ########################################################################################
+    print_info("DNS ZONETRANSFER:")
+    print_info("="*48)
+
+    ns_servers = []
+    ns_answer = dns.resolver.query(domain, 'NS')
+    for server in ns_answer:
+        print_info(f"[*] FOUND NS: {server}")
+        ip_answer = dns.resolver.query(server.target, 'A')
+
+        for ip in ip_answer:
+            try:
+                zone = dns.zone.from_xfr(dns.query.xfr(str(ip), domain))
+                for host in zone:
+                    print_error(f"[-] FOUND HOST: {host}.{domain}")
+
+            except Exception as e:
+                print_found(f"[+] NS {server} REFUSED ZONE TRANSFER")
+                continue
+
+
     ########################################################################################
     # Banner grabbing - check for Server or other inform. desclosure - e.g.: PHP version
     ########################################################################################
-    print_info("GETTING THE HEADER INFORMATION:")
+    print_info("HTTP HEADER INFORMATION:")
     print_info("="*48)
     r = session.get(URL)
     for header in r.headers.keys():
-        inform_disc_headers = "Server,Set-Cookie".split(",")
-        sec_headers = "Strict-Transport-Security,Cache-Control,Set-Cookie,Content-Security-Policy".split(",")
-        info_headers = "Content-Type,Transfer-Encoding,Content-Encoding".split(",")
+        inform_disc_headers = ["Server", "Set-Cookie"]
+        sec_headers = ["Strict-Transport-Security", "Cache-Control", "Set-Cookie", "Content-Security-Policy"]
+        info_headers = ["Content-Type", "Transfer-Encoding", "Content-Encoding"]
 
         # Information Disclosure
         for elem in inform_disc_headers:
@@ -125,7 +176,7 @@ with open(f"{domain}.log", "w", encoding="UTF-8") as log_file:
         # Other useful informations
         for elem in info_headers:
             if header.lower() == elem.lower():
-                print_info(f"[ ] INFO:               {elem}: {r.headers[elem]}")
+                print_info(f"[*] INFO:               {elem}: {r.headers[elem]}")
     
     print_info()
 
@@ -133,7 +184,7 @@ with open(f"{domain}.log", "w", encoding="UTF-8") as log_file:
     ########################################################################################
     # Get SSL cert subdomain list
     ########################################################################################
-    print_info("GETTING SUBDOMAIN INFORMATION:")
+    print_info("SUBDOMAINS:")
     print_info("="*48)
 
     url = f"https://crt.sh/?q={domain}"
@@ -150,9 +201,13 @@ with open(f"{domain}.log", "w", encoding="UTF-8") as log_file:
             for subdom in subdom_list:
                 subdomains.add(subdom)
 
-        subdomains.remove(domain)
+        try:
+            subdomains.remove(domain)
+        except:
+            pass
+
         for subdom in sorted(subdomains):
-            print_info(f"[ ] SUDOMAIN FOUND: {subdom}")
+            print_info(f"[*] SUDOMAIN FOUND: {subdom}")
 
     else:
         print_error("[-] ERROR - COULD NOT GET SSL CERTIFICATE LIST")
